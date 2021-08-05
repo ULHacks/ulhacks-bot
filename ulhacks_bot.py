12dc96a8ce2805b5840e0a84d18bdfeb14553119
George Zhang <geetransit@gmail.com>
2021-08-05 14:21:37 -0400

    Apply workaround to prevent ignored RuntimeErrors

--- ulhacks_bot.py
+++ ulhacks_bot.py
@@ -1,17 +1,37 @@
-"""Entry point to the ULHacks Bot"""
+"""Entry point to the ULHacks Bot
 
+For more info on why we have both a run and a _run function, see
+https://github.com/GeeTransit/joshgone/blob/main/joshgone.py and its comments.
+
+"""
 import os
 from discord.ext import commands
 
 command_prefix = commands.when_mentioned_or("$")
 
-def run(token, **bot_kwargs):
+def _run(token, **bot_kwargs):
     bot = commands.Bot(**bot_kwargs)
 
     bot.load_extension("extensions.info")
     bot.load_extension("extensions.admin")
 
+    close = bot.loop.close
+    bot.loop.close = lambda: None
+
     bot.run(token)
+
+    bot.loop.close = close
+    return bot.loop
+
+def run(token, **bot_kwargs):
+    """Runs the ULHacks Bot with the provided token and bot options"""
+    loop = _run(token, **bot_kwargs)
+
+    try:
+        loop.call_later(0.1, loop.stop)
+        loop.run_forever()
+    finally:
+        loop.close()
 
 if __name__ == "__main__":
     run(
