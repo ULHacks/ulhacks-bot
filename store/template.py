57cdfc13bb8c9a503ba4ff9f22d44872d59949e4
George Zhang <geetransit@gmail.com>
2021-08-07 17:43:35 -0400

    Add a keys method to the store interface

--- store/template.py
+++ store/template.py
@@ -1,9 +1,14 @@
 """Provides an abstract interface for a key-value storage"""
 
+from collections.abc import AsyncIterator
+
 class Store:
     async def set(self, key: str, value: str) -> None:
         raise NotImplementedError
     async def get(self, key: str) -> str:
+        raise NotImplementedError
+    # Used to move data between stores. Can be slow / inefficient
+    async def keys(self) -> AsyncIterator[str]:
         raise NotImplementedError
 
 def setup(bot) -> None:
