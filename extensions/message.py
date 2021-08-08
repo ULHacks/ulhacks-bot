fc864a814849f5f3790a1fdfb906eb926ec7d9a0
George Zhang <geetransit@gmail.com>
2021-08-08 19:48:20 -0400

    Only allow requests from members in the server

--- extensions/message.py
+++ extensions/message.py
@@ -5,12 +5,20 @@ from discord.ext import commands
 class Message(commands.Cog):
     TYPES = tuple("message register hacker".split())
 
-    def __init__(self, bot, *, message_channel_id):
+    def __init__(self, bot, *, guild_id, message_channel_id, invite_link):
         self.bot = bot
+        self.guild_id = guild_id
+        self.guild = None
         self.message_channel_id = message_channel_id
         self.message_channel = None
+        self.invite_link = invite_link
 
     async def cog_before_invoke(self, ctx):
+        # Ensures the server exists
+        if self.guild is None:
+            self.guild = self.bot.get_guild(self.guild_id)
+            if self.guild is None:
+                raise RuntimeError("cannot get server")
         # Ensures the message_channel exists
         if self.message_channel is None:
             self.message_channel = self.bot.get_channel(self.message_channel_id)
@@ -21,6 +29,12 @@ class Message(commands.Cog):
     @commands.cooldown(1, 5, commands.BucketType.user)
     async def message(self, ctx, *, text=""):
         """Send a message to the organizers who will get back to you ASAP"""
+        if not self.guild.get_member(ctx.author.id):
+            content = "You aren't in the ULHacks server."
+            if self.invite_link:
+                content += f" Join using this link: {self.invite_link}."
+            await ctx.send(content)
+            return
         if not text:
             await ctx.send("Please add a message to your request")
             return
@@ -37,6 +51,12 @@ class Message(commands.Cog):
     @commands.command()
     async def register(self, ctx, *, text=""):
         """Send a message to the organizers to notify them of your role"""
+        if not self.guild.get_member(ctx.author.id):
+            content = "You aren't in the ULHacks server."
+            if self.invite_link:
+                content += f" Join using this link: {self.invite_link}."
+            await ctx.send(content)
+            return
         if await self.bot.store.get(f"message/register/{ctx.author.id}"):
             await ctx.send(
                 "You already registered. If you have any questions, please use"
@@ -61,6 +81,12 @@ class Message(commands.Cog):
     @commands.command()
     async def hacker(self, ctx):
         """Notify the organizers of your participation"""
+        if not self.guild.get_member(ctx.author.id):
+            content = "You aren't in the ULHacks server."
+            if self.invite_link:
+                content += f" Join using this link: {self.invite_link}."
+            await ctx.send(content)
+            return
         if await self.bot.store.get(f"message/hacker/{ctx.author.id}"):
             await ctx.send(
                 "You have already sent a verification request with `$hacker`."
@@ -102,4 +128,9 @@ class Message(commands.Cog):
             await ctx.send(f"Updated the {type_} prefix to: {prefix}")
 
 def setup(bot):
-    bot.add_cog(Message(bot, message_channel_id=873653291614085180))
+    bot.add_cog(Message(
+        bot,
+        guild_id=871391148239880252,
+        message_channel_id=873653291614085180,
+        invite_link="https://discord.gg/An26dB3WBP",
+    ))
