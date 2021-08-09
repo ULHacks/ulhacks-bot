0ffff602c706a8bf2f5bcf4d80cd47c89bcc0fc4
George Zhang <geetransit@gmail.com>
2021-08-08 21:20:03 -0400

    Move the interface to __init__.py

--- store/__init__.py
+++ store/__init__.py
@@ -1,3 +1,15 @@
-"""A package with implementations for key-value storages"""
+"""A package with implementations for key-value storages
 
-from .template import *
+An abstract class is provided for implementations to follow.
+
+"""
+from collections.abc import AsyncIterator
+
+class Store:
+    async def set(self, key: str, value: str) -> None:
+        raise NotImplementedError
+    async def get(self, key: str) -> str:
+        raise NotImplementedError
+    # Used to move data between stores. Can be slow / inefficient
+    async def keys(self) -> AsyncIterator[str]:
+        raise NotImplementedError
